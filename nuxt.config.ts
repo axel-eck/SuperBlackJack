d97979de9770b22aa95af1bc6d21a6d3cd6ea971
Axel Eckenberg <axel.eckenberg@yahoo.fr>
2026-02-27 01:44:32 +0000

    :fire: Remove pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
-  modules: ['@nuxt/eslint', '@pinia/nuxt'],
+  modules: ['@nuxt/eslint'],
   postcss: {
     plugins: {
       "@tailwindcss/postcss": {},
