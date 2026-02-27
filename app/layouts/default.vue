d37b2128a3b9e2e0b7441d11f75033e3c3eddc41
Axel Eckenberg <axel.eckenberg@yahoo.fr>
2026-02-27 00:36:28 +0000

    :sparkles: Add speed insight

---
+++ app/layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
+
+<template>
+  <div class="w-screen h-screen flex flex-col items-center justify-center">
+    <SpeedInsights />
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
